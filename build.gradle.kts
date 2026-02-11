039b9bbe12070cebf5b8afdde4c80b08a0fe9d80
Phodal Huang <h@phodal.com>
2026-02-11 09:28:04 +0800

    test: Add Java plugin test framework to platform configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -105,6 +105,8 @@ dependencies {
         bundledModules(providers.gradleProperty("platformBundledModules").map { it.split(',') })
 
         testFramework(TestFrameworkType.Platform)
+        testFramework(TestFrameworkType.Plugin.Java)
+        testFramework(TestFrameworkType.Bundled)
     }
 }
 
